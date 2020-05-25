9809aad98c48ef08dceb6678c2ad5892ef4b026b
Daniel Santos De Sousa <64164438+DaniFTT@users.noreply.github.com>
2020-05-25 15:28:12 -0300

    Update Worker.java

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -74,12 +74,10 @@ public class Worker {
 		Calendar cal = Calendar.getInstance();
 		
 		for (HourContract c : contracts) {
-			
 			cal.setTime(c.getDate());
 			int c_year = cal.get(Calendar.YEAR);
-		    int c_month = 1 + cal.get(Calendar.MONTH);
-		    
-			if(year == c_year && month == c_month) {
+			int c_month = 1 + cal.get(Calendar.MONTH);
+		        if(year == c_year && month == c_month) {
 				sum += c.totalValue();
 			}
 		}
